2b06793f68b34773c3f8e3ea4db002a37c5b9350
McMerph2 <mcmerph@protonmail.com>
2023-05-26 10:08:02 +0700

    Handle unaccounted customers

--- src/db/watch.ts
+++ src/db/watch.ts
@@ -1,45 +1,94 @@
+import type { Collection, UpdateResult } from "mongodb";
 import { ObjectId } from "mongodb";
 
 import { withCustomersCollections } from "../db/withCustomersCollections";
 import { anonymiseCustomer } from "../domain/anonymiseCustomer";
-import { type CustomerWithId } from "./CustomerWithId";
+import type { CustomerWithId } from "./CustomerWithId";
+
+const logCustomer = (
+  id: string,
+  acknowledged: boolean,
+  success: string,
+  fail: string
+) => {
+  if (acknowledged) {
+    console.log(`Successfully ${success} customer#${id}`);
+  } else {
+    console.error(`Failed to ${fail} customer#${id} customer(s)`);
+  }
+};
+
+const handleUnaccounted = async (
+  originalCollection: Collection,
+  anonymousCollection: Collection
+) => {
+  const lastAnonymousCustomer = await anonymousCollection
+    .find()
+    .sort({ _id: -1 })
+    .limit(1)
+    .next();
+  if (!lastAnonymousCustomer) return;
+
+  console.log("Start handling unaccounted clients");
+  const unaccounted = originalCollection
+    .find({
+      _id: {
+        $lte: ObjectId.createFromTime(Math.floor(Date.now() / 1000)),
+        $gt: lastAnonymousCustomer._id,
+      },
+    })
+    .sort({ _id: 1 });
+  for await (const customer of unaccounted) {
+    const result = await anonymousCollection.insertOne(
+      anonymiseCustomer(customer as CustomerWithId)
+    );
+    logCustomer(
+      result.insertedId.toHexString(),
+      result.acknowledged,
+      "inserted unaccounted",
+      "insert unaccounted"
+    );
+  }
+
+  console.log("Finish handling unaccounted clients");
+};
 
 // TODO Add batching
-// TODO The application can be restarted. In the event of a restart, it should continue where it left off, not missing any changes that happened while the application was offline.
 export const watch = withCustomersCollections(
   async (originalCollection, anonymousCollection) => {
+    void handleUnaccounted(originalCollection, anonymousCollection);
+
     const changeStream = originalCollection.watch([], {
       fullDocument: "updateLookup",
     });
-
     for await (const change of changeStream) {
       if (change.operationType === "insert") {
         const customer = change.fullDocument;
         const result = await anonymousCollection.insertOne(
           anonymiseCustomer(customer as CustomerWithId)
         );
-        const id = result.insertedId.toHexString();
-        if (result.acknowledged) {
-          console.log(`Successfully inserted customer#${id}`);
-        } else {
-          console.error(`Failed to insert customer#${id} customer(s)`);
-        }
+        logCustomer(
+          result.insertedId.toHexString(),
+          result.acknowledged,
+          "inserted new",
+          "insert new"
+        );
       } else if (change.operationType === "update") {
         const customer = change.fullDocument;
         if (!customer) throw new Error("Failed to get client");
         if (!(customer["_id"] instanceof ObjectId))
           throw new Error("No customer ID");
 
-        const result = await anonymousCollection.replaceOne(
+        const result = (await anonymousCollection.replaceOne(
           { _id: customer["_id"] },
           anonymiseCustomer(customer as CustomerWithId)
+        )) as UpdateResult;
+        logCustomer(
+          customer["_id"].toHexString(),
+          result.acknowledged,
+          "updated",
+          "update"
         );
-        const id = customer["_id"].toHexString();
-        if (result.acknowledged) {
-          console.log(`Successfully updated customer#${id}`);
-        } else {
-          console.error(`Failed to update customer#${id} customer(s)`);
-        }
       }
     }
   }
