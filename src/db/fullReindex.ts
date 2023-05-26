5658278781dacb964d390f19e53707de7c29ab37
McMerph2 <mcmerph@protonmail.com>
2023-05-26 14:56:08 +0700

    Ignore duplicated keys in --full-reindex mode

--- src/db/fullReindex.ts
+++ src/db/fullReindex.ts
@@ -1,4 +1,5 @@
 import type { ObjectId } from "mongodb";
+import { MongoServerError } from "mongodb";
 
 import { anonymiseCustomer } from "../domain/anonymiseCustomer";
 import type { Customer } from "../domain/Customer";
@@ -9,14 +10,20 @@ type CustomerWithId = Customer & { _id: ObjectId };
 export const fullReindex = withCustomersCollections(
   async (originalCollection, anonymousCollection) => {
     for await (const customer of originalCollection.find()) {
-      const result = await anonymousCollection.insertOne(
-        anonymiseCustomer(customer as CustomerWithId)
-      );
-      const id = result.insertedId.toHexString();
-      if (result.acknowledged) {
-        console.debug(`Successfully inserted customer#${id}`);
-      } else {
-        console.error(`Failed to insert customer#${id} customer(s)`);
+      try {
+        const result = await anonymousCollection.insertOne(
+          anonymiseCustomer(customer as CustomerWithId)
+        );
+        const id = result.insertedId.toHexString();
+        if (result.acknowledged) {
+          console.debug(`Successfully inserted customer#${id}`);
+        } else {
+          console.error(`Failed to insert customer#${id} customer(s)`);
+        }
+      } catch (error) {
+        const isDuplicateKey =
+          error instanceof MongoServerError && error.code === 11000;
+        if (!isDuplicateKey) throw error;
       }
     }
   }
